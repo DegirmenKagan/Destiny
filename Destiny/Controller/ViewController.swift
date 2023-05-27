9957f6bfdfb6621785dbc4fd02aa7455f75a15e0
Kağan Değirmen <kagandegirmen@gmail.com>
2023-05-27 16:48:27 +0300

    button, label connections fixed

--- Destiny/Controller/ViewController.swift
+++ Destiny/Controller/ViewController.swift
@@ -9,6 +9,9 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var choiceButton1: UIButton!
+    @IBOutlet weak var choiceButton2: UIButton!
+    @IBOutlet weak var storyLabel: UILabel!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.

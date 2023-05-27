db7b8f016dd052ab45793a9818773d13544c93b7
Kağan Değirmen <kagandegirmen@gmail.com>
2023-05-27 17:45:25 +0300

    Controller, View > implementing models into controller

--- Destiny/Controller/ViewController.swift
+++ Destiny/Controller/ViewController.swift
@@ -8,15 +8,30 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
     @IBOutlet weak var choiceButton1: UIButton!
     @IBOutlet weak var choiceButton2: UIButton!
     @IBOutlet weak var storyLabel: UILabel!
+    
+    var storyLogic = StoryLogic()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        updateUI()
         // Do any additional setup after loading the view.
     }
+    
+    @IBAction func choiceMade(_ sender: UIButton) {
+        storyLogic.nextStory(userChoice: sender.currentTitle!)
 
-
+        updateUI()
+    }
+    
+    func updateUI(){
+        storyLabel.text = storyLogic.getTitle()
+        let choices = storyLogic.getChoices()
+        choiceButton1.setTitle(choices[0], for: .normal)
+        choiceButton2.setTitle(choices[1], for: .normal)
+    }
 }
 

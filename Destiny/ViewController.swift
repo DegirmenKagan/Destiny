373433430dbc433034b002d0200f74b2203dbf52
Kağan Değirmen <kagandegirmen@gmail.com>
2023-05-27 13:57:44 +0300

    Initial Commit

---
+++ Destiny/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Destiny
+//
+//  Created by Kağan Değirmen on 27.05.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
